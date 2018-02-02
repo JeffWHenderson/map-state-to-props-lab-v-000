e2bc1d5cc162862ed352b2e0a112c9e35aa2858a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-02 05:23:39 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends Component {
     return (
       <div className="App">
         <UserInput store={this.props.store}/>
-          <ConnectedUsers store={this.props.store}
+          <ConnectedUsers store={this.props.store} />
       </div>
     );
   }
